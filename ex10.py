11fa806263d094b610f8a64f480dfeaf536dd5a8
unknown <Sokleap.Seng@PNC.LAN>
2024-08-28 15:32:15 +0700

    new

---
+++ ex10.py
@@ -0,0 +1,24 @@
+# EX10.Show product name that has maximum price
+
+# products = [
+#     {"name": "Apple", "price": 20},
+#     {"name": "Orange", "price": 24},
+# ]
+
+def sum(products):
+    name = ""
+    max = 0
+    for i in range(len(products)):
+        if max == 0:
+            max = products[0]["price"]
+            name = products[i]["name"]
+        elif products[i]["price"] > max:
+            max = products[i]["price"]
+            name = products[i]["name"]
+
+    return name
+name_products = [
+    {"name": "Apple", "price": 20},
+    {"name": "Orange", "price": 24},
+]
+print(sum(name_products))
